9f481ba2b7188c3d131a89c7cda24acacaac0bf9
Alastair M. Robinson <robinsonb5@gmail.com>
2019-08-11 10:03:17 +0100

    Started work on simulator

---
+++ Assembler/assembler.pp
@@ -0,0 +1,52 @@
+#define IMW0(x) (x&0x3f)
+#define IMW1(x) ((x>>6)&0x3f)
+#define IMW2(x) ((x>>12)&0x3f)
+#define IMW3(x) ((x>>18)&0x3f)
+#define IMW4(x) ((x>>24)&0x3f)
+#define IMW5(x) ((x>>30)&0x3)
+
+
+#define cond .byte 0x0 +
+#define mt .byte 0x08 +
+#define mr .byte 0x10 +
+#define exg .byte 0x18 +
+#define ldi .byte 0x20 +
+#define st .byte 0x28 +
+#define ld .byte 0x30 +
+#define sti .byte 0x38 +
+#define add .byte 0x40 +
+#define cmp .byte 0x48 +
+#define ldinc .byte 0x50 +
+#define sub .byte 0x58 +
+#define stdec .byte 0x60 +
+#define and .byte 0x68 +
+#define or .byte 0x70 +
+#define xor .byte 0x78 +
+#define addt .byte 0x80 +
+#define shl .byte 0x88 +
+#define asr .byte 0x90 +
+#define lsr .byte 0x98 +
+#define ror .byte 0xa0 +
+#define rorc .byte 0xa8 +
+#define stbinc .byte 0xb0 +
+#define ldbinc .byte 0xb8 +
+#define li .byte 0xc0 +
+
+#define r0 0
+#define r1 1
+#define r2 2
+#define r3 3
+#define r4 4
+#define r5 5
+#define r6 6
+#define r7 7
+
+#define NEX 0
+#define SGT 1
+#define EQ 2
+#define GE 3
+#define SLT 4
+#define NEQ 5
+#define LE 6
+#define EX 7
+
---
+++ Sim/832sim.cpp
@@ -0,0 +1,519 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <getopt.h>
+
+#include "binaryblob.h"
+#include "debug.h"
+
+#define STACKSIZE 1024
+int STACKOFFSET=0;
+
+
+class EightThirtyTwoMemory
+{
+	public:
+	EightThirtyTwoMemory(int ramsize) : uartbusyctr(0), ram(0), ramsize(ramsize), uartin(0)
+	{
+		ram=new int[ramsize/4];
+	}
+	virtual ~EightThirtyTwoMemory()
+	{
+		if(ram)
+			delete[] ram;
+	}
+	virtual void SetUARTIn(const char *c)
+	{
+		uartin=c;
+	}
+	virtual int Read(unsigned int addr)
+	{
+		switch(addr)
+		{
+			case 0xffffffc4:
+				Debug[WARN] << std::endl << "Reading from SPI_CS" << std::endl << std::endl;
+				break;
+			case 0xffffffc8:
+				Debug[WARN] << std::endl << "Reading from SPI" << std::endl << std::endl;
+				break;
+			case 0xffffffcc:
+				Debug[WARN] << std::endl << "Reading from SPI_PUMP" << std::endl << std::endl;
+				break;
+			case 0xda8000:
+			case 0xffffff84:
+			case 0xffffffc0:
+				Debug[WARN] << std::endl << "Reading from UART" << std::endl << std::endl;
+				if(uartbusyctr)
+				{
+					--uartbusyctr;
+					return(0);
+				}
+				else
+				{
+					int result;
+					uartbusyctr=1;	// Make the UART pretend to be busy for the next n cycles
+					if(uartin)
+						result=0x300 | (unsigned char)*uartin++;	// Received byte ready...
+					else
+						result=0x300 | (std::cin.get()&0xff);
+
+					if(result==0x300)	// End of string?
+						uartin=0;
+
+//					if(std::cin.eof())
+//						throw "End of input data";
+
+					return(result);
+				}
+				break;
+			default:
+				Debug[TRACE] << std::endl << "Reading from RAM" << addr << std::endl;
+//				if(addr<ramsize)
+					return(ram[(addr&(ramsize-1))/4]);
+		}
+		return(0);
+	}
+	virtual void Write(unsigned int addr,int v)
+	{
+		switch(addr)
+		{
+			case 0xda8000:
+				v>>=16;
+			case 0xffffff84:
+			case 0xffffffc0:
+				if(char(v))
+				{
+					Debug[WARN] << std::endl << "Writing " << char(v) << " to UART" << std::endl << std::endl;
+					std::cout << char(v);
+				}
+				else
+				{
+					Debug[WARN] << std::endl << "Writing (nul) to UART" << std::endl << std::endl;
+					std::cout << "(nul)";
+				}
+				break;
+
+			case 0xffffff88:
+				Debug[WARN] << std::endl << "Setting UART divisor to " << v << std::endl << std::endl;
+				break;
+
+			case 0xffffff8C:
+				Debug[WARN] << std::endl << "Writing to overlay register: " << v << std::endl << std::endl;
+				break;
+
+			case 0xffffff90:
+				Debug[WARN] << std::endl << "Writing " << v << " to HEX display" << std::endl << std::endl;
+				break;
+
+			case 0xffffffc4:
+				Debug[WARN] << std::endl << "Setting spi_cs line " << (v&1 ? "low" : "high" ) << std::endl << std::endl;
+				break;
+
+			case 0xffffffc8:
+				Debug[WARN] << std::endl << "Writing " << v << " to SPI" << std::endl << std::endl;
+				break;
+
+			case 0xffffffcc:
+				Debug[WARN] << std::endl << "Writing " << v << " to SPI_pump" << std::endl << std::endl;
+				break;
+
+			case 0xfffffffc:
+				Debug[WARN] << std::endl << "Breadcrumb " << std::endl << std::endl;
+				break;
+
+			default:
+//				if(addr<ramsize)
+//				{
+					Debug[TRACE] << std::endl << "Writing " << v << " to RAM " << addr << std::endl;
+					ram[(addr&(ramsize-1))/4]=v;
+//				}
+		}
+	}
+	virtual unsigned char &operator[](const int idx)=0;
+	protected:
+	int uartbusyctr;
+	int *ram;
+	int ramsize;
+	const char *uartin;
+};
+
+
+// EightThirtyTwoProgram overlays the program loaded from disk onto a fixed-size memory block.
+// Accesses within the program are sent directly to the binary blob.
+// Accesses beyond the program are passed to the EightThirtyTwoMemory superclass.
+
+class EightThirtyTwoProgram : public BinaryBlob, public EightThirtyTwoMemory
+{
+	public:
+	EightThirtyTwoProgram(const char *filename, int ramsize=8*1024*1024) : BinaryBlob(filename), EightThirtyTwoMemory(ramsize), base(0)
+	{
+	}
+	~EightThirtyTwoProgram()
+	{
+	}
+	virtual int Read(unsigned int addr)
+	{
+		if(((addr-base)>=0) && ((addr-base)<(size-3)))
+		{
+			int r=(*this)[addr]<<24;
+			r|=(*this)[addr+1]<<16;
+			r|=(*this)[addr+2]<<8;
+			r|=(*this)[addr+3];
+			return(r);
+		}
+		return(EightThirtyTwoMemory::Read(addr));
+	}
+	virtual void Write(unsigned int addr,int v)
+	{
+		if(((addr-base)>=0) && ((addr-base)<(size-3)))
+		{
+			(*this)[addr]=(v>>24)&255;
+			(*this)[addr+1]=(v>>16)&255;
+			(*this)[addr+2]=(v>>8)&255;
+			(*this)[addr+3]=v&255;
+		}
+		else
+			EightThirtyTwoMemory::Write(addr,v);
+	}
+	void SetBase(int base)
+	{
+		this->base=base;
+	}
+	unsigned char &operator[](int idx)
+	{
+		return(BinaryBlob::operator[](idx));
+	}
+	protected:
+	int base;
+};
+
+
+
+
+
+class EightThirtyTwoSim 
+{
+	public:
+	EightThirtyTwoSim() : initpc(0), steps(-1)
+	{		
+	}
+
+	~EightThirtyTwoSim()
+	{
+	}
+
+	int ParseOptions(int argc,char *argv[])
+	{
+		static struct option long_options[] =
+		{
+			{"help",no_argument,NULL,'h'},
+			{"steps",required_argument,NULL,'s'},
+			{"offset",required_argument,NULL,'o'},
+			{"report",required_argument,NULL,'r'},
+			{0, 0, 0, 0}
+		};
+		bool offset=false;
+		int stackbit=30;
+		bool stackboot=false;
+
+		while(1)
+		{
+			int c;
+			c = getopt_long(argc,argv,"hs:r:o:bm",long_options,NULL);
+			if(c==-1)
+				break;
+			switch (c)
+			{
+				case 'h':
+					printf("Usage: %s [options] <UART input text>\n",argv[0]);
+					printf("    -h --help\t  display this message\n");
+					printf("    -s --steps\t  Simulate a specific number of steps (default: indefinite)\n");
+					printf("    -r --report\t  set reporting level - 0 for silent, 4 for verbose\n");
+					printf("    -o --offsetstack\t  specify base address for stack RAM. Zero by default,\n");
+					printf("\t\t  specified as a bit number, so 30=0x40000000, etc.\n");
+					break;
+				case 'o':
+					offset=true;
+					stackbit=atoi(optarg);
+					break;
+				case 's':
+					steps=atoi(optarg);
+					break;
+				case 'r':
+					Debug.SetLevel(DebugLevel(atoi(optarg)));
+					break;
+			}
+		}
+
+		return(optind);
+	}
+
+	
+
+	int GetOpcode(EightThirtyTwoMemory &prg, int pc)
+	{
+		int t=prg.Read(pc&~3);
+		int opcode=t>>((3-(pc&3))<<3);
+		return(opcode&0xff);
+	}
+
+
+	void Run(EightThirtyTwoProgram &prg)
+	{
+		Debug[WARN] << "Starting simulation" << std::endl;
+		Debug[ERROR] << std::hex << std::endl;
+		for(int i=0;i<7;++i)
+			regfile[i]=0;
+		regfile[7]=initpc;
+		zero=0; carry=0;
+
+		bool run=true;
+
+		bool immediate_continuation=false;
+
+		while(run)
+		{
+			int nextpc;
+			int opcode;
+			int operand;
+			int operim;
+			int t;
+			long long t2;
+
+			std::stringstream mnem;
+			mnem << std::hex;
+
+			opcode=GetOpcode(prg,regfile[7]);
+			operand=opcode&0x7;
+			operim=opcode&0x3f;
+			opcode&=0xfc;
+
+			nextpc=regfile[7]+1;
+			regfile[7]=nextpc;			
+
+			if((opcode&0xc0)==0xc0)
+			{
+				if(immediate_continuation)
+				{
+					temp<<=6;
+					temp|=operim;
+					mnem<<("li (cont) ");
+					mnem << operim;
+				}
+				else
+				{
+					temp=operim;
+					if(operim&0x20)
+						temp|=0xffffffc0;
+					mnem<<("li ");
+					mnem<< operim;
+					immediate_continuation=true;
+				}
+			}
+			else
+			{
+				immediate_continuation=false;
+				switch(opcode)
+				{
+					case 0x00: // cond
+						// FIXME - match against zero and carry, set cond flag accordingly.
+						mnem << ("cond ") << operand;
+						break;
+
+					case 0x08: // mt
+						temp=regfile[operand];
+						mnem << ("mt ") << operand;
+						break;
+
+					case 0x10: // mr
+						regfile[operand]=temp;
+						mnem << ("mr ") << operand;
+						break;
+
+					case 0x18: // exg
+						t=regfile[operand];
+						regfile[operand]=temp;
+						temp=t;
+						mnem << ("exg ") << operand;
+						break;
+
+					case 0x20: // ldi
+						temp=prg.Read((regfile[operand]+regfile[5])&0xfffffffc);
+						break;
+
+					case 0x28: // st
+						prg.Write(regfile[operand]&0xfffffffc,temp);
+						break;
+
+					case 0x30: // ld
+						temp=prg.Read(regfile[operand]&0xfffffffc);
+						break;
+
+					case 0x38: // sti
+						prg.Write((regfile[operand]+regfile[5])&0xfffffffc,temp);
+						break;
+
+					case 0x40: // add
+						t2=regfile[operand]+temp;
+						carry=(t2>>32)&1;
+						regfile[operand]=t2;
+						zero=(t2&0xffffffff)==0;
+						break;
+
+					case 0x48: // cmp
+						t2=regfile[operand]-temp;
+						carry=(t2>>32)&1;
+						zero=(t2&0xffffffff)==0;
+						break;
+
+					case 0x50: // ldinc
+						temp=prg.Read(regfile[operand]&0xfffffffc);
+						regfile[operand]+=4;
+						break;
+
+					case 0x58: // sub
+						t2=regfile[operand]+temp;
+						carry=(t2>>32)&1;
+						regfile[operand]=t2;
+						zero=(t2&0xffffffff)==0;
+						break;
+
+					case 0x60: // stdec
+						prg.Write(regfile[operand]&0xfffffffc,temp);
+						regfile[operand]-=4;
+						break;
+
+					case 0x68: // and
+						regfile[operand]&=temp;
+						carry=0;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0x70: // or
+						regfile[operand]|=temp;
+						carry=0;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0x78: // xor
+						regfile[operand]^=temp;
+						carry=0;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0x80: // addt;
+						t=regfile[operand];
+						t2=regfile[operand]+temp;
+						carry=(t2>>32)&1;
+						regfile[operand]=t2;
+						zero=(t2&0xffffffff)==0;
+						temp=t;
+						break;
+
+					case 0x88: // shl
+						t2=regfile[operand]<<(temp-1);
+						carry=t2>>32;
+						regfile[operand]<<=temp;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0x90: // asr
+						carry=regfile[operand]>>(temp-1);
+						carry&=1;
+						t=regfile[operand];
+						t>>=temp;
+						regfile[operand]=t;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0x98: // lsr
+						carry=regfile[operand]>>(temp-1);
+						carry&=1;
+						regfile[operand]>>=temp;
+						zero=regfile[operand]==0;
+						break;
+
+					case 0xa0: // ror
+						throw "ror not yet implemented\n";
+						break;
+
+					case 0xa8: // rorc
+						throw "rorc not yet implemented\n";
+						break;
+
+					case 0xb0: // stbinc
+						prg[regfile[operand]]=temp&0xff;
+						regfile[operand]++;
+						break;
+
+					case 0xb8: // ldbinc
+						temp=prg[regfile[operand]];
+						regfile[operand]++;
+						break;
+				}
+
+				if(steps>0)
+					run=(--steps)!=0;
+//				else
+//					run=true;
+
+			}
+			Debug[TRACE] << "r7: " << regfile[7] << "\tOp: " << opcode << ", " << mnem.str() << "\n\t\t";
+			DumpRegs();
+			Debug[TRACE] << std::endl;
+		}
+	}
+	void DumpRegs()
+	{
+		Debug[TRACE] << "Temp: " << temp << ", ";
+		for(int i=0;i<7;++i)
+		{
+			Debug[TRACE] << "r" << i << ": " << regfile[i] << ", ";
+		}
+		Debug[TRACE] << "Z: " << zero << ", C: " << carry;
+	}
+	protected:
+	unsigned int regfile[8];
+	int temp;
+	int zero;
+	int carry;
+	int initpc;
+	int steps;
+};
+
+
+int main(int argc, char **argv)
+{
+	try
+	{
+		Debug.SetLevel(TRACE);
+		if(argc>1)
+		{
+			int i;
+			char *uartin=0;
+			EightThirtyTwoSim sim;
+			i=sim.ParseOptions(argc,argv);
+			if(i<argc)
+			{
+				EightThirtyTwoProgram prg(argv[i++]);
+				if(i<argc)
+				{
+					Debug[TRACE] << "Setting uartin to " << argv[i] << std::endl;
+					prg.SetUARTIn(argv[i++]);
+				}
+				else
+				{
+					Debug[TRACE] << "All arguments used: " << i << ", " << argc << std::endl;
+				}
+				sim.Run(prg);
+			}
+		}
+	}
+	catch(const char *err)
+	{
+		std::cerr << "Error: " << err << std::endl;
+	}
+	return(0);
+}
+
