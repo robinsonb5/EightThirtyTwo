d70201fd2c05830ca9f30c1b9d3ad19b2d72b144
Alastair M. Robinson <robinsonb5@gmail.com>
2019-09-22 16:29:09 +0100

    Started to implement opcodes.

---
+++ RTL/eightthirtytwo_shifter.vhd
@@ -0,0 +1,88 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Shift and rotate unit
+
+entity eightthirtytwo_shifter is
+port(
+	clk : in std_logic;
+	reset_n : in std_logic;
+	d : in std_logic_vector(31 downto 0);
+	q : out std_logic_vector(31 downto 0);
+	shift : in std_logic_vector(4 downto 0);
+	immediate : in std_logic_vector(5 downto 0);
+	right_left : in std_logic; -- 1 for right, 0 for left
+	sgn : in std_logic;	-- Right shift only - logical (0) or arithmetic (1)?
+	rotate : in std_logic;
+	req : in std_logic;
+	busy : out std_logic
+);
+end entity;
+
+architecture rtl of eightthirtytwo_shifter is
+	signal result : std_logic_vector(31 downto 0);
+	signal count : unsigned(4 downto 0);
+	signal signbit : std_logic;
+begin
+
+	q<=result;
+	signbit<=sgn and d(31);
+
+	process(clk,req,reset_n)
+	begin
+
+		if reset_n='0' then
+			count<=(others=>'0');
+		elsif rising_edge(clk) then
+
+			busy<='1';
+
+			if req='1' then
+				count<=unsigned(shift);
+				result<=d;
+			else
+				if count="00000" then
+					busy<='0';
+				else
+					if rotate='1' then
+						if count(4 downto 2)/="000" then
+							result(31 downto 28)<=result(3 downto 0);
+							result(27 downto 0)<=result(31 downto 4);
+							count<=count-4;
+						else
+							result(31)<=result(0);
+							result(30 downto 0)<=result(31 downto 1);
+							count<=count-1;
+						end if;
+					elsif right_left='1' then -- shift right, both logical and arithmetic
+						if count(4 downto 2)/="000" then
+							result(31 downto 28)<=signbit & signbit & signbit & signbit;
+							result(27 downto 0)<=result(31 downto 4);
+							count<=count-4;
+						else
+							result(31)<=signbit;
+							result(30 downto 0)<=result(31 downto 1);
+							count<=count-1;
+						end if;
+					elsif right_left='0' then -- shift left - always shift in zeros
+						if count>6 then	-- six bits, incorporate immediate data if we have any.
+							result(31 downto 6)<=result(25 downto 0);
+							result(5 downto 0)<=immediate;
+							count<=count-6;
+						else
+							result(31 downto 1)<=result(30 downto 0);
+							result(0)<='0';
+							count<=count-1;
+						end if;
+					end if;
+
+				end if;				
+			end if;
+
+		end if;
+	end process;
+
+
+
+end architecture;
---
+++ testbench/cpu_tb.vhd
@@ -0,0 +1,136 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.rom_pkg.all;
+
+entity cpu_tb is
+end cpu_tb;
+
+architecture behaviour of cpu_tb
+is
+	constant clk_period : time := 10 ns;
+	signal clk : std_logic;
+
+	signal reset_n : std_logic;
+	signal pc :std_logic_vector(31 downto 0);
+	signal pc_q :std_logic_vector(31 downto 0);
+	signal pc_req : std_logic;
+	signal pc_next : std_logic;
+
+	signal ls_addr : std_logic_vector(31 downto 0);
+	signal ls_byte : std_logic;
+	signal ls_halfword : std_logic;
+	signal ls_req : std_logic;
+	signal ls_wr : std_logic;
+	signal ls_ack : std_logic;
+
+
+	signal ram_addr : std_logic_vector(31 downto 2);
+	signal from_ram : std_logic_vector(31 downto 0);
+	signal to_ram : std_logic_vector(31 downto 0);
+	signal ram_wr : std_logic;
+	signal ram_bytesel : std_logic_vector(3 downto 0);
+	signal ram_req : std_logic;
+	signal ram_ack : std_logic;
+
+	type tbstates is (RESET,INIT,MAIN,LOAD);
+	signal tbstate : tbstates:=RESET;
+
+	signal romin : fromROM;
+	signal romout : toROM;
+
+begin
+
+	rom : entity work.helloworld_rom
+	port map(
+		clk => clk,
+		from_soc => romout,
+		to_soc => romin
+	);
+
+	romout.MemAAddr<=ram_addr(15 downto 2);
+	romout.MemAWrite<=to_ram;
+	romout.MemAWriteEnable<=ram_wr and ram_req;
+	romout.MemAByteSel<=ram_bytesel;
+	from_ram<=romin.MemARead;
+
+	cpu : entity work.eightthirtytwo_cpu
+	port map
+	(
+		clk => clk,
+		reset_n => reset_n,
+
+		-- cpu fetch interface
+
+		addr => ram_addr,
+		d => from_ram,
+		q => to_ram,
+		bytesel => ram_bytesel,
+		wr => ram_wr,
+		req => ram_req,
+		ack => ram_ack
+	);
+
+  -- Clock process definition
+  clk_process: process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+  end process;
+
+	process(clk,ls_ack)
+	begin
+
+		if rising_edge(clk) then
+
+			pc_req<='0';
+			reset_n<='1';
+			pc_next<='0';
+			ram_ack<='0';
+
+			ls_byte<='0';
+			ls_halfword<='0';
+			ls_wr<='0';
+			ls_req<='0';
+
+			if ram_req='1' then
+				ram_ack<='1';
+			end if;
+
+			case tbstate is
+				when RESET =>
+					reset_n<='0';
+					tbstate<=INIT;
+				when INIT =>
+					pc<=(others => '0');
+					pc_req<='1';
+					tbstate<=MAIN;
+				when MAIN =>
+					if pc_q=X"00000010" then
+						tbstate<=LOAD;
+					else
+						pc_next<='1';
+						tbstate<=MAIN;
+					end if;
+
+				when LOAD =>
+					ls_addr<=X"00000013";
+					ls_byte<='1';
+					ls_wr<='0';
+					ls_req<='1' and not ls_ack;
+					if ls_ack='1' then
+						tbstate<=MAIN;
+					end if;
+
+			end case;
+
+		end if;
+
+	end process;
+
+end behaviour;
+
