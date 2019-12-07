3a160e539ca428020266aa6b0e596958293b1482
Alastair M. Robinson <robinsonb5@gmail.com>
2019-12-07 21:40:44 +0000

    Updated string.h

--- Lib/string.h
+++ Lib/string.h
@@ -5,45 +5,42 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-#include <klibc/extern.h>
-#include <stddef.h>
-
-__extern void *memccpy(void *, const void *, int, size_t);
-__extern void *memchr(const void *, int, size_t);
-__extern void *memrchr(const void *, int, size_t);
-__extern int memcmp(const void *, const void *, size_t);
-__extern void *memcpy(void *, const void *, size_t);
-__extern void *memmove(void *, const void *, size_t);
-__extern void *memset(void *, int, size_t);
-__extern void *memmem(const void *, size_t, const void *, size_t);
-__extern void memswap(void *, void *, size_t);
-__extern void bzero(void *, size_t);
-__extern int strcasecmp(const char *, const char *);
-__extern int strncasecmp(const char *, const char *, size_t);
-__extern char *strcat(char *, const char *);
-__extern char *strchr(const char *, int);
-__extern char *index(const char *, int);
-__extern char *strrchr(const char *, int);
-__extern char *rindex(const char *, int);
-__extern int strcmp(__reg("r2") const char *, __reg("r1") const char *);
-__extern char *strcpy(char *, const char *);
-__extern size_t strcspn(const char *, const char *);
-__extern char *strdup(const char *);
-__extern char *strndup(const char *, size_t);
-__extern char *strerror(int);
-__extern char *strsignal(int);
-__extern size_t strlen(const char *);
-__extern size_t strnlen(const char *, size_t);
-__extern char *strncat(char *, const char *, size_t);
-__extern size_t strlcat(char *, const char *, size_t);
-__extern int strncmp(const char *, const char *, size_t);
-__extern char *strncpy(char *, const char *, size_t);
-__extern size_t strlcpy(char *, const char *, size_t);
-__extern char *strpbrk(const char *, const char *);
-__extern char *strsep(char **, const char *);
-__extern size_t strspn(const char *, const char *);
-__extern char *strstr(const char *, const char *);
-__extern char *strtok(char *, const char *);
-__extern char *strtok_r(char *, const char *, char **);
+ void *memccpy(void *, const void *, int, size_t);
+ void *memchr(const void *, int, size_t);
+ void *memrchr(const void *, int, size_t);
+ int memcmp(const void *, const void *, size_t);
+ void *memcpy(void *, const void *, size_t);
+ void *memmove(void *, const void *, size_t);
+ void *memset(void *, int, size_t);
+ void *memmem(const void *, size_t, const void *, size_t);
+ void memswap(void *, void *, size_t);
+ void bzero(void *, size_t);
+ int strcasecmp(const char *, const char *);
+ int strncasecmp(const char *, const char *, size_t);
+ char *strcat(char *, const char *);
+ char *strchr(const char *, int);
+ char *index(const char *, int);
+ char *strrchr(const char *, int);
+ char *rindex(const char *, int);
+ int strcmp(__reg("r2") const char *, __reg("r1") const char *);
+ char *strcpy(char *, const char *);
+ size_t strcspn(const char *, const char *);
+ char *strdup(const char *);
+ char *strndup(const char *, size_t);
+ char *strerror(int);
+ char *strsignal(int);
+ size_t strlen(const char *);
+ size_t strnlen(const char *, size_t);
+ char *strncat(char *, const char *, size_t);
+ size_t strlcat(char *, const char *, size_t);
+ int strncmp(const char *, const char *, size_t);
+ char *strncpy(char *, const char *, size_t);
+ size_t strlcpy(char *, const char *, size_t);
+ char *strpbrk(const char *, const char *);
+ char *strsep(char **, const char *);
+ size_t strspn(const char *, const char *);
+ char *strstr(const char *, const char *);
+ char *strtok(char *, const char *);
+ char *strtok_r(char *, const char *, char **);
 
 #endif				/* _STRING_H */
