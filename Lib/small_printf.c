33c8bb68398b63be5440614b69aacbe634ab9e48
Alastair M. Robinson <robinsonb5@gmail.com>
2019-12-19 07:45:48 +0000

    Added pre-seeded gtkw file to testbenches.

--- Lib/small_printf.c
+++ Lib/small_printf.c
@@ -3,46 +3,12 @@
 
 static char temp[80];
 
-int _cvt(int val, char *buf, int radix)
+static int _cvt(int val, char *buf, int radix)
 {
-#ifdef PRINTF_HEX_ONLY
-	int c;
-	int i;
-	int nz=0;
-	if(val<0)
-	{
-		putchar('-');
-		val=-val;
-	}
-	if(val)
-	{
-		for(i=0;i<8;++i)
-		{
-			c=(val>>28)&0xf;
-			val<<=4;
-			if(c)
-				nz=1;	// Non-zero?  Start printing then.
-			if(c>9)
-				c+='A'-10;
-			else
-				c+='0';
-			if(nz)	// If we've encountered only zeroes so far we don't print.
-				putchar(c);
-		}
-	}
-	else
-		putchar('0');
-	return(0);
-#else
     char *cp = temp;
 	const char *digits="0123456789ABCDEF";
     int length = 0;
 
-	if(val<0)
-	{
-		putchar('-');
-		val=-val;
-	}
     if (val == 0) {
         /* Special case */
         *cp++ = '0';
@@ -55,13 +21,11 @@ int _cvt(int val, char *buf, int radix)
         }
     }
     while (cp != temp) {
-//		putchar((unsigned char)(*(cp-1)));
         *buf++ = *--cp;
         length++;
     }
     *buf = '\0';
     return (length);
-#endif
 }
 
 static char vpfbuf[sizeof(long)*8];
@@ -71,8 +35,8 @@ int small_printf(const char *fmt, ...)
     va_list ap;
     int ret=0;
 	unsigned int c;
-	int nextfmt=0;
 	int length;
+	int nextfmt=0;
 
     va_start(ap, fmt);
 
@@ -80,33 +44,48 @@ int small_printf(const char *fmt, ...)
 	{
 		if(nextfmt)
 		{
-			nextfmt=0;
+			int val;
+			int base=0;
 			length=0;
+			nextfmt=0;
 	        // Process output
 	        switch (c) {
 			    case 'd':
-			        ret+=length = _cvt(va_arg(ap,int), vpfbuf, 10);
-					puts(vpfbuf);
+				case 'u':
+					base=10;
 				    break;
+			    case 'p':
 			    case 'x':
-			        ret+=length = _cvt(va_arg(ap,int), vpfbuf, 16);
-					puts(vpfbuf);
+					base=16;
 				    break;
 			    case 's':
 			        ret+=puts(va_arg(ap, char *));
 			        break;
 				case 'l':
+				case '0':
 					nextfmt=1;
 					break;
 			    case 'c':
 			        putchar(va_arg(ap, int /*char*/));
 					ret++;
-			        continue;
+			        break;
 			    default:
 			        putchar('%');
 			        putchar(c);
-			        continue;
+					ret++;
+			        break;
 	        }
+			if(base)
+			{
+				val=va_arg(ap,int);
+				if(c=='d' && val<0)
+				{
+					putchar('-');
+					val=-val;
+				}
+		        ret+=length = _cvt(val, vpfbuf, base);
+				puts(vpfbuf);
+			}
 		}
 		else
 		{
