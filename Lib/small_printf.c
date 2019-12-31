01b3cfc6c05e07cdd4fa5f3708191c504e6b3ecc
Alastair M. Robinson <robinsonb5@gmail.com>
2019-12-31 18:09:21 +0000

    We have working sprint / snprintf.

--- Lib/small_printf.c
+++ Lib/small_printf.c
@@ -1,36 +1,50 @@
 #include "uart.h"
-#include "stdarg.h"
+#include <stdarg.h>
+#include <stddef.h>
 
 static char temp[16];
 
-static int _cvt(int val, char *buf, unsigned int radix)
-{
-    char *cp = temp;
-	const char *digits="0123456789ABCDEF";
-    int length = 0;
+typedef int (*pf_outfunc)(int c,void *ofdata);
 
-    if (val == 0) {
-        /* Special case */
-        *cp++ = '0';
-    } else {
-        while (val) {
+static int _cvt(int val, unsigned int radix,pf_outfunc f,void *ofdata)
+{
+	char *cp = temp;
+	const char *digits="0123456789ABCDEF";
+	int length = 0;
+
+	if (val == 0) {
+		/* Special case */
+		*cp++ = '0';
+	} else {
+		while (val) {
 			unsigned int c;
 			c=val%radix;
-            *cp++ = digits[c];
-            val /= radix;
-        }
-    }
-    while (cp != temp) {
-        *buf++ = *--cp;
-        length++;
-    }
-    *buf = '\0';
-    return (length);
+			*cp++ = digits[c];
+			val /= radix;
+		}
+	}
+	while (cp != temp) {
+		f(*--cp,ofdata);
+		length++;
+	}
+	return (length);
 }
 
-static char vpfbuf[sizeof(long)*8];
+static int _pfputs(const char *s,pf_outfunc f, void *ofdata)
+{
+	int result=-1;
+	int c;
+	int cont=1;
+	while(c=*s++)
+	{
+		++result;
+		if(f(c,ofdata)!=c)
+			return(result);
+	}
+	return(result);
+}
 
-__weak int vprintf(const char *fmt, va_list ap)
+__weak int _printfcore(const char *fmt,va_list ap,pf_outfunc f,void *ofdata)
 {
     int ret=0;
 	unsigned int c;
@@ -56,19 +70,19 @@ __weak int vprintf(const char *fmt, va_list ap)
 					base=16;
 				    break;
 			    case 's':
-			        ret+=puts(va_arg(ap, char *));
+			        ret+=_pfputs(va_arg(ap, char *),f,ofdata);
 			        break;
 				case 'l':
 				case '0':
 					nextfmt=1;
 					break;
 			    case 'c':
-			        putchar(va_arg(ap, int /*char*/));
+			        f(va_arg(ap, int /*char*/),ofdata);
 					ret++;
 			        break;
 			    default:
-			        putchar('%');
-			        putchar(c);
+					f('%',ofdata);
+			        f(c,ofdata);
 					ret++;
 			        break;
 	        }
@@ -80,8 +94,7 @@ __weak int vprintf(const char *fmt, va_list ap)
 					putchar('-');
 					val=-val;
 				}
-		        ret+=length = _cvt(val, vpfbuf, base);
-				puts(vpfbuf);
+		        ret+=length = _cvt(val, base,f,ofdata);
 			}
 		}
 		else
@@ -89,19 +102,78 @@ __weak int vprintf(const char *fmt, va_list ap)
 			if(c=='%')
 				nextfmt=1;
 			else
-				putchar(c);
+				f(c,ofdata);
 		}
 	}
     return (ret);
 }
 
-__weak int printf(const char *fmt, ...)
+
+int vprintf(const char *fmt, va_list ap)
+{
+	return(_printfcore(fmt,ap,(pf_outfunc)&putchar,0));	// Extraneous parameter should be OK on 832.
+}
+
+int printf(const char *fmt, ...)
 {
 	int ret;
     va_list ap;
 	va_start(ap,fmt);
-	ret=vprintf(fmt,ap);
+	ret=_printfcore(fmt,ap,(pf_outfunc)&putchar,0);
 	va_end(ap);
 	return(ret);
+}
+
+struct _fpwcdata
+{
+	char *buf;
+	size_t len;
+};
+
+static int _fpwritechar(int c,void *ud)
+{
+	struct _fpwcdata *d=(struct _fpwcdata*)ud;
+	if(d->len)
+	{
+		--d->len;
+		*d->buf++=c;	// FIXME - check size modifier
+		return(c);
+	}
+	else
+		return(0);
+}
+
+int sprintf(char *buf,const char *fmt, ...)
+{
+	int ret;
+    va_list ap;
+	struct _fpwcdata ud;
+	ud.buf=buf;
+	ud.len=-1;
+	va_start(ap,fmt);
+	ret=_printfcore(fmt,ap,_fpwritechar,&ud);
+	va_end(ap);
+	return(ret);
+}
+
+int snprintf(char *buf,size_t size,const char *fmt, ...)
+{
+	int ret;
+    va_list ap;
+	struct _fpwcdata ud;
+	ud.buf=buf;
+	ud.len=size;
+	va_start(ap,fmt);
+	ret=_printfcore(fmt,ap,_fpwritechar,&ud);
+	va_end(ap);
+	return(ret);
+}
+
+int vsprintf(char *buf,const char *fmt, va_list ap)
+{
+	struct _fpwcdata ud;
+	ud.buf=buf;
+	ud.len=-1;
+	return(_printfcore(fmt,ap,_fpwritechar,&ud));
 }
 
--- include/stdio.h
+++ include/stdio.h
@@ -1,2 +1,8 @@
-#include "small_printf.h"
+#ifndef STDIO_H
+#define STDIO_H
+
+int printf(const char *fmt, ...);
+int sprintf(char *buf,const char *fmt, ...);
+
+#endif
 
