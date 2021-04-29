1aa7ae335bb195a6ca982a53af3233573a2517bd
Alastair M. Robinson <robinsonb5@gmail.com>
2021-04-29 20:31:04 +0100

    Tidied up license headers.

--- RTL/eightthirtytwo_aligner.vhd
+++ RTL/eightthirtytwo_aligner.vhd
@@ -8,10 +8,6 @@
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 
--- If you modify EightThirtyTwo you may extend this exception to your version,
--- but you are not obliged to do so. If you do not wish to do so, delete this exception
--- statement from your version.
-
 -- EightThirtyTwo is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
